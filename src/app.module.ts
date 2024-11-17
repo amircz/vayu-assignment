2495c098882d2a51ebaf3c74861785f9623eb78c
amircz <amircz1997@gmail.com>
2024-11-17 18:14:38 +0200

    backend works and unused code deleted

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import {Module} from '@nestjs/common';
-import {AppController} from './app.controller';
-import {AppService} from './app.service';
 import {UsersModule} from './users/users.module';
 import {UsersController} from "./users/users.controller";
 import {UsersService} from "./users/users.service";
@@ -10,8 +8,8 @@ import {GroupsService} from "./groups/groups.service";
 
 @Module({
     imports: [UsersModule, GroupsModule],
-    controllers: [AppController, UsersController],
-    providers: [AppService, UsersService, PrismaService, GroupsService],
+    controllers: [UsersController],
+    providers: [UsersService, PrismaService, GroupsService],
 })
 export class AppModule {
 }
